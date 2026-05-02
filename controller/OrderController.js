7898a389cb71039b89636fd9e478a8ca3f0c203e
SayumiPerera <sayumiperera18@gmail.com>
2026-05-02 22:53:11 +0530

    order control-clean form,click order row update

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -20,3 +20,22 @@ const loadOrderTbl = () => {
 }
 
 
+//------------------------- Clean Order Form ------------------------------
+const cleanOrderForm = () => {
+    $('#order_reset_btn').click();
+}
+
+
+//------------------------- Click on Order Row ------------------------------
+$('#order_tbody').on('click', 'tr', function () {
+    let order_obj = getOrderDataByIndex($(this).index());
+
+    $('#order_id_input').val(order_obj.id);
+    $('#order_customerName_input').val(order_obj.customerName);
+    $('#order_itemList_input').val(order_obj.itemList);
+    $('#order_qty_input').val(order_obj.qty);
+    $('#order_totalPrice_input').val(order_obj.totalPrice);
+    $('#order_date_input').val(order_obj.date);
+})
+
+
